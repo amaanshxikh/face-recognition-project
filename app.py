1b2a333caa2e7ed81119a33d879a77bfbe3ade5b
Amaan Shaikh <amaan.qsx@gmail.com>
2025-04-24 11:09:09 +0530

    minor changes

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
 
 if count == 0:
     st.write("Count is zero")
-elif count % 3 == 0 and count % 5 == 0:
-    st.write("FizzBuzz")
-elif count % 3 == 0:
-    st.write("Fizz")
-elif count % 5 == 0:
-    st.write("Buzz")
-else:
+# elif count % 3 == 0 and count % 5 == 0:
+#     st.write("FizzBuzz")
+# elif count % 3 == 0:
+#     st.write("Fizz")
+# elif count % 5 == 0:
+#     st.write("Buzz")
+# else:
     st.write(f"Count: {count}")
 
 
